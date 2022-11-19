bc8429facef8abae5eb59776493528b8b5890e44
HappySolutions <heba.odesk.tasks@gmail.com>
2022-11-19 15:42:41 +0200

    Create a new Login destination

---
+++ app/src/main/java/com/example/shoestoretask/screens/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.example.shoestoretask.screens.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.example.shoestoretask.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: ViewDataBinding? =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login,
+                container, false)
+        return binding?.root
+    }
+
+}
