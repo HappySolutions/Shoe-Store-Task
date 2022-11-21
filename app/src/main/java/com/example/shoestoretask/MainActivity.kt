9701758fe8cfa86498e16ef7b59c39dfd6f09b45
HappySolutions <heba.odesk.tasks@gmail.com>
2022-11-21 12:10:19 +0200

    try to figure out what went wrong

--- app/src/main/java/com/example/shoestoretask/MainActivity.kt
+++ app/src/main/java/com/example/shoestoretask/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.shoestoretask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.example.shoestoretask.R
+import com.example.shoestoretask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ViewDataBinding>(this, R.layout.activity_main)
+       // DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
     }
+
 }
