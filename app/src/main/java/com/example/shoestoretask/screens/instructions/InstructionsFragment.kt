e813eec43e56e37d0f1ed811ec0d9721d317496d
HappySolutions <heba.odesk.tasks@gmail.com>
2022-11-22 17:56:47 +0200

    update to fix databinding

--- app/src/main/java/com/example/shoestoretask/screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/shoestoretask/screens/instructions/InstructionsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.shoestoretask.screens.instructions
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,26 +9,26 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.shoestoretask.R
 import com.example.shoestoretask.databinding.FragmentInstructionsBinding
 import com.example.shoestoretask.databinding.FragmentShoeDetailsBinding
+import com.example.shoestoretask.databinding.FragmentWelcomeBinding
+import com.example.shoestoretask.screens.welcome.WelcomeFragmentDirections
 
 
 class InstructionsFragment : Fragment() {
+//    private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val binding: FragmentShoeDetailsBinding? =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_details,
                 container, false)
-//        binding.instNxtBtn.setOnClickListener(
-//            Navigation.createNavigateOnClickListener(R.id.action_instructionsFragment_to_shoeListFragment)
-  //      )
-
         return binding?.root
+
     }
 
 }
